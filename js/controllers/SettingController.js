06a13da655f57e965a6eb766d89037626df79331
Vincent Qiu <vincent.qiu.china@gmail.com>
2016-03-09 17:17:49 +0100

    make some changes

--- js/controllers/SettingController.js
+++ js/controllers/SettingController.js
@@ -3,9 +3,11 @@
 
 	controlsModule.controller('SettingController', ['$state', '$scope', '$timeout', function($state, $scope, $timeout){
 		$scope.isSettingDisplayed = false;
+		$scope.isSettingFixed = false;
 
-		var __showSettingPanel = function(){
+		var __showSettingPanel = function(isFixed){
 			$scope.isSettingDisplayed = true;
+			$scope.isSettingFixed = isFixed;
 			$timeout(function(){
 				$scope.$emit('SETTING_PANEL_SHOWN');
 			}, 200);
@@ -13,6 +15,7 @@
 
 		var __hideSettingPanel = function(){
 			$scope.isSettingDisplayed = false;
+			$scope.isSettingFixed = false;
 			$timeout(function(){
 				$scope.$emit('SETTING_PANEL_HIDDEN');
 			}, 200);
@@ -22,12 +25,24 @@
 			if($scope.isSettingDisplayed){
 				__hideSettingPanel()
 			} else {
-				__showSettingPanel();
+				__showSettingPanel(true);
+			}
+		};
+
+		$scope.hoverOnSetting = function(){
+			if(!$scope.isSettingFixed){
+				__showSettingPanel(false);
+			}
+		};
+
+		$scope.hoverOffSetting = function(){
+			if(!$scope.isSettingFixed){
+				__hideSettingPanel();
 			}
 		};
 
 		$scope.$on('SHOW_SETTING_PANEL', function(event, msg){
-			__showSettingPanel();
+			__showSettingPanel(true);
 		});
 
 		var fetchControlSettingData = function(){
